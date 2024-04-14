746fb25035cfb9a09da8cf33cd6b414ac84b8766
sjma21 <143322037+sjma21@users.noreply.github.com>
2024-04-14 11:17:30 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+let imgbox=document.getElementById("imgbox");
+let qrimage=document.getElementById("qrimage");
+let qrtext= document.getElementById("qrtext");
+function generateQr(){
+    if(qrtext.value.length>0){
+        qrimage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +qrtext.value;
+        imgbox.classList.add("show-img");
+    }else{
+        qrtext.classList.add("error");
+        setTimeout(()=>{
+            qrtext.classList.remove("error");
+        },1000);
+    }
+ }
